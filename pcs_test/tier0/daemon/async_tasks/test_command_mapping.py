ba56d6f1db37bae436a06c5304b8b0a064bfb558
Peter Romancik <promanci@redhat.com>
2026-04-22 21:08:44 +0200

    WIP: test for detection of unions

--- pcs_test/tier0/daemon/async_tasks/test_command_mapping.py
+++ pcs_test/tier0/daemon/async_tasks/test_command_mapping.py
@@ -4,9 +4,11 @@ from dataclasses import (
     is_dataclass,
 )
 from enum import EnumType
+from types import NoneType, UnionType
 from typing import (
     Container,
     Iterable,
+    Union,
     get_args,
     get_origin,
     get_type_hints,
@@ -15,6 +17,8 @@ from unittest import TestCase
 
 from pcs.common.interface.dto import DTO_TYPE_HOOKS_MAP
 from pcs.daemon.async_tasks.worker.command_mapping import COMMAND_MAP
+
+_PRIMITIVE_TYPES = frozenset({str, int, float, bool, NoneType})
 
 
 def prohibited_types_used(_type, prohibited_types):
@@ -81,6 +85,39 @@ def _find_disallowed_types(_type, allowed_types, _seen=None):
     return disallowed
 
 
+def _find_disallowed_unions(_type, _seen=None):
+    if _seen is None:
+        _seen = set()
+    type_id = id(_type)
+    if type_id in _seen:
+        return set()
+    _seen.add(type_id)
+
+    disallowed = set()
+    origin = get_origin(_type)
+
+    if origin is Union or origin is UnionType:
+        args = get_args(_type)
+        non_none = [a for a in args if a is not NoneType]
+        if len(non_none) == 1:
+            disallowed.update(_find_disallowed_unions(non_none[0], _seen))
+        elif not all(t in _PRIMITIVE_TYPES for t in non_none):
+            disallowed.add(_type)
+            for arg in args:
+                disallowed.update(_find_disallowed_unions(arg, _seen))
+    elif origin is not None:
+        for arg in get_args(_type):
+            disallowed.update(_find_disallowed_unions(arg, _seen))
+
+    if is_dataclass(_type):
+        type_hints = get_type_hints(_type)
+        for field in fields(_type):
+            disallowed.update(
+                _find_disallowed_unions(type_hints[field.name], _seen)
+            )
+    return disallowed
+
+
 class DaciteTypingCompatibilityTest(TestCase):
     def test_all(self):
         prohibited_types = (Iterable, Container)
@@ -120,4 +157,20 @@ class DaciteTypingCompatibilityTest(TestCase):
                     "Add the missing type(s) to DTO_TYPE_HOOKS_MAP "
                     "and update FromDictConversion tests in "
                     "test_dto.py accordingly.",
+                )
+
+    def test_check_no_disallowed_unions_in_commands(self):
+        for cmd_name, cmd in COMMAND_MAP.items():
+            for param in list(inspect.signature(cmd.cmd).parameters.values())[
+                1:
+            ]:
+                if param.annotation == inspect.Parameter.empty:
+                    continue
+                disallowed = _find_disallowed_unions(param.annotation)
+                self.assertFalse(
+                    disallowed,
+                    f"Disallowed Union type(s) {disallowed} found in "
+                    f"command: {cmd_name}; argument: {param}. "
+                    "Unions must be Optional[T] or a union of only primitive "
+                    "types (str, int, float, bool, None).",
                 )
